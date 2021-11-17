f74924aab057cd7eeb06b0c3317b27833a5aa16b
thiagophellipe <94555914+thiagophellipe@users.noreply.github.com>
2021-11-17 14:47:21 -0300

    commit inicial
    O primeiro commit do site

---
+++ js/main.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($){
+	//FIXED HEADER
+	window.onscroll = function(){
+		if(window.pageYOffset > 140)
+			$('#header').addClass("active");
+	} else {
+		$('#header').removeClass("active")
+	}
+});
+//OWL
+$(".owl-carousel").owlCarousel({
+	loop: true;
+	margin: 30;
+	autoplay: false;
+	autoplayTimeout: 6000;
+	dots: true;
+	lazyload: true;
+	nav: false;
+	responsive: {
+		0: {
+			items: 1;
+		};
+		600: {
+			items: 1;
+		};
+		1000: {
+			items: 2;
+		};
+	};
+});
